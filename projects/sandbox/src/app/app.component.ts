899b5e8dc901d885b30972c488fe37d1b2b1708d
RubyZz1 <bdarrousez@sfr.fr>
2023-11-17 13:56:26 +0100

    first commit

---
+++ projects/sandbox/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: ` <h1>Hello world 2</h1> `,
+  styles: [],
+})
+export class AppComponent {}
